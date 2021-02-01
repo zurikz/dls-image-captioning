e3c0cf277cf87d066e668e469b556a3c4d8d2b19
Zurab Nurmagomedov <zurabnurmag@gmail.com>
2021-02-01 21:25:08 +0300

    Initial commit

---
+++ notebooks/base part/model.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence
+from torch.nn.utils.rnn import pad_packed_sequence
+
+class CaptionNet(nn.Module):
+    def __init__(self, vocab, hidden_size, num_layers, lstm_dropout=0.3,
+                 fc_dropout=0.3, embedding_dim=300, cnn_feature_size=2048):
+        super().__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+
+        self.fc_h = nn.Linear(cnn_feature_size, hidden_size * num_layers)
+        self.fc_c = nn.Linear(cnn_feature_size, hidden_size * num_layers)
+        self.bn = nn.BatchNorm1d(cnn_feature_size)
+
+        self.embedding = nn.Embedding(num_embeddings=len(vocab), 
+                                      embedding_dim=embedding_dim,
+                                      padding_idx=vocab.pad_idx)
+
+        self.lstm = nn.LSTM(input_size=embedding_dim,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            dropout=lstm_dropout,
+                            batch_first=True)
+
+        self.fc_out = nn.Linear(hidden_size, len(vocab))
+        self.dropout = nn.Dropout(fc_dropout)
+
+    def init_hidden_states(self, image_vectors):
+        """
+        (bs, cnn_feature_size) -fc-> (bs, hidden_size * num_layers)
+                          -reshape-> (num_layers, bs, hidden_size)
+        """
+        image_vectors = self.bn(image_vectors)
+        h = self.fc_h(image_vectors)
+        c = self.fc_c(image_vectors)
+        h = h.reshape((self.num_layers, -1, self.hidden_size))
+        c = c.reshape((self.num_layers, -1, self.hidden_size))
+        return h, c
+
+    def forward(self, image_vectors, padded_captions, lenghts):
+        """
+        :param image_vectors: (bs, cnn_feature_size)
+        :param padded_captions: (bs, max_seq_len)
+        :param lenghts: (bs)
+        """
+        # embedded_captions: (bs, max_seq_len, embedding_dim)
+        embedded_captions = self.embedding(padded_captions)
+        packed_captions = pack_padded_sequence(
+            embedded_captions,
+            lengths=lenghts,
+            batch_first=True,
+            enforce_sorted=False
+        )
+
+        h0, c0 = self.init_hidden_states(image_vectors)
+        output, _ = self.lstm(packed_captions, (h0, c0))
+        output = pad_packed_sequence(output, batch_first=True)
+
+        # output[0]: (bs, max_seq_len, hidden_size)
+        logits = self.fc_out(self.dropout(output[0]))
+
+        return logits
---
+++ notebooks/base part/utils.py
@@ -0,0 +1,109 @@
+import json
+import numpy as np
+from collections import Counter
+
+import torch
+from torch.utils.data import Dataset
+from torch.nn.utils.rnn import pad_sequence
+
+
+class Vocab:
+    def __init__(self, captions_path: str, min_freq=5):
+        self.captions = json.load(open(captions_path))
+        self.word2idx, self.idx2word = self.build_vocab(min_freq)
+        self.unk_idx = self.word2idx['<unk>']
+        self.pad_idx = self.word2idx['<pad>']
+
+    def build_vocab(self, min_freq):    
+        word_freq = Counter()
+
+        for idx in range(len(self.captions)):
+            for sent_idx in range(len(self.captions[idx])):
+                word_freq.update((self.captions[idx][sent_idx]).split())
+
+        words = [w for w in word_freq.keys() if word_freq[w] > min_freq]
+        special_tokens = ['<pad>', '<sos>', '<eos>', '<unk>']
+        word2idx = {word: idx for idx, word in enumerate(special_tokens + words)}
+        idx2word = {idx: word for idx, word in enumerate(special_tokens + words)}
+        return word2idx, idx2word
+
+    def stoi(self, words):
+        if isinstance(words, str):
+            word = words
+            return self.word2idx[word] if word in self.word2idx \
+                                       else self.unk_idx
+        elif isinstance(words, list):
+            return [self.word2idx[word] if word in self.word2idx
+                    else self.unk_idx for word in words]
+
+    def itos(self, idx):
+        if isinstance(idx, int):
+            return self.idx2word[idx]
+        elif isinstance(idx, list):
+            return [self.idx2word[i] for i in idx]
+        
+    def __len__(self):
+        return len(self.word2idx)
+
+
+class CaptionDataset(Dataset):
+    def __init__(self, split: str, vocab: Vocab,
+                 img_codes_path: str, captions_path: str):
+        self.img_codes = np.load(img_codes_path)
+        self.captions = json.load(open(captions_path))
+
+        assert split in {'train', 'val', 'test'}
+
+        idx = len(self.img_codes) - 10000
+        if split == 'train':
+            self.img_codes = self.img_codes[:idx]
+            self.captions = self.captions[:idx]
+        if split == 'val':
+            self.img_codes = self.img_codes[idx:(idx + 5000)]
+            self.captions = self.captions[idx:(idx + 5000)]
+        if split == 'test':
+            self.img_codes = self.img_codes[(idx + 5000):]
+            self.captions = self.captions[(idx + 5000):]
+
+        self.sort_dataset()
+        self.encode_sentences(vocab)
+
+    def encode_sentences(self, vocab, max_len=20):
+        for idx in range(len(self.captions)):
+            for sent_idx in range(len(self.captions[idx])):
+                sent = '<sos> ' + self.captions[idx][sent_idx] + ' <eos>'
+                encoded_sent = vocab.stoi(sent.split())
+                if len(encoded_sent) > max_len:
+                    encoded_sent = encoded_sent[:max_len]
+                    encoded_sent[-1] = vocab.stoi('<eos>')
+                self.captions[idx][sent_idx] = encoded_sent
+
+    def sort_dataset(self):
+        self.captions, self.img_codes = zip(
+            *sorted(
+                zip(self.captions, self.img_codes),
+                key=lambda cap_img: sum([len(cap) for cap in cap_img[0]])
+            )
+        )
+
+    def __getitem__(self, idx):
+        img = self.img_codes[idx]
+        captions = self.captions[idx]
+        random = np.random.randint(low=0, high=len(captions))
+        caption = captions[random]
+        return (torch.tensor(img),
+                torch.tensor(caption),
+                torch.tensor(len(caption)))
+
+    def __len__(self):
+        return len(self.captions)
+
+
+def pad_collate_fn(batch):
+    image_vectors, captions, lenghts = zip(*batch)
+
+    padded_captions = pad_sequence(captions, batch_first=True)
+    image_vectors = torch.stack(image_vectors)
+    lenghts = torch.stack(lenghts)
+
+    return image_vectors, padded_captions, lenghts
